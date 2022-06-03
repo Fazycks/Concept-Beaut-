fa50458d1a7ef6d8b697be10ddb262fb0d3544a4
Alessio Vanini <yolessioaninio@gmail.com>
2022-06-03 16:31:48 +0200

    init

---
+++ Cartes-des-services/main.js
@@ -0,0 +1,40 @@
+function mainpied2() {
+    if(document.getElementById('mainpied').style.display == "none") {
+        document.getElementById('mainpied').style.display = "block";
+        document.getElementById('mainpied1').innerHTML = "Masquer les prestations &nbsp; &nbsp; &nbsp; &#x1F53A;";
+    } else {
+        document.getElementById('mainpied').style.display = "none";
+        document.getElementById('mainpied1').innerHTML = "Voir les 10 autres prestations &nbsp; &nbsp; &nbsp; &#128315;";
+    }
+
+}
+
+
+function cils2() {
+    if(document.getElementById('cils').style.display == "none") {
+        document.getElementById('cils').style.display = "block";
+        document.getElementById('cils1').innerHTML = "Masquer les prestations &nbsp; &nbsp; &nbsp; &#x1F53A;";
+    } else {
+        document.getElementById('cils').style.display = "none";
+        document.getElementById('cils1').innerHTML = "Voir les 8 autres prestations &nbsp; &nbsp; &nbsp; &#128315;";
+    }
+
+}
+
+
+function epilation2() {
+    if(document.getElementById('epilation').style.display == "none") {
+        document.getElementById('epilation').style.display = "block";
+        document.getElementById('epilation1').innerHTML = "Masquer les prestations &nbsp; &nbsp; &nbsp; &#x1F53A;";
+    } else {
+        document.getElementById('epilation').style.display = "none";
+        document.getElementById('epilation1').innerHTML = "Voir les 5 autres prestations &nbsp; &nbsp; &nbsp; &#128315;";
+        
+    }
+
+}
+
+
+
+
+
